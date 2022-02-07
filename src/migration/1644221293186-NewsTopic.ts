46e831939390099b19c110a1b712dd13bfb60fd1
sulthonuladib <adibcyber4rt@gmail.com>
2022-02-07 15:25:21 +0700

    feat(migration: update)

--- src/migration/1644221293186-NewsTopic.ts
+++ src/migration/1644221293186-NewsTopic.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class NewsTopic1644202835315 implements MigrationInterface {
-    name = 'NewsTopic1644202835315'
+export class NewsTopic1644221293186 implements MigrationInterface {
+    name = 'NewsTopic1644221293186'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "topic" ALTER COLUMN "created_at" SET DEFAULT 'now()'`);
@@ -13,10 +13,10 @@ export class NewsTopic1644202835315 implements MigrationInterface {
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "news" ALTER COLUMN "deleted_at" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "news" ALTER COLUMN "updated_at" SET DEFAULT '2022-02-07 02:59:59.587362'`);
-        await queryRunner.query(`ALTER TABLE "news" ALTER COLUMN "created_at" SET DEFAULT '2022-02-07 02:59:59.587362'`);
-        await queryRunner.query(`ALTER TABLE "topic" ALTER COLUMN "updated_at" SET DEFAULT '2022-02-07 02:59:59.587362'`);
-        await queryRunner.query(`ALTER TABLE "topic" ALTER COLUMN "created_at" SET DEFAULT '2022-02-07 02:59:59.587362'`);
+        await queryRunner.query(`ALTER TABLE "news" ALTER COLUMN "updated_at" SET DEFAULT '2022-02-07 08:07:29.131279'`);
+        await queryRunner.query(`ALTER TABLE "news" ALTER COLUMN "created_at" SET DEFAULT '2022-02-07 08:07:29.131279'`);
+        await queryRunner.query(`ALTER TABLE "topic" ALTER COLUMN "updated_at" SET DEFAULT '2022-02-07 08:07:29.131279'`);
+        await queryRunner.query(`ALTER TABLE "topic" ALTER COLUMN "created_at" SET DEFAULT '2022-02-07 08:07:29.131279'`);
     }
 
 }
