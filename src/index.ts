3880d534fb5a1970a275c6e364ba1fe66f28b7df
sulthonuladib <adibcyber4rt@gmail.com>
2022-02-07 12:07:51 +0700

    feat(main): classificate running server

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,55 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import {Routes} from "./routes";
+// import "reflect-metadata";
+// import { createConnection } from "typeorm";
+// import * as express from "express";
+// import * as bodyParser from "body-parser";
+// import { Request, Response } from "express";
+// import { Routes } from "./routes";
 
-const appInfo = require("../package.json");
+// import {api} from "./api";
+// const appInfo = require("../package.json");
 
-const app = express();
-app.use(bodyParser.json());
+// const app = express();
+// app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(appInfo)
-});
+// app.get('/', (req: Request, res: Response) => {
+//     res.send(appInfo)
+// });
 
-createConnection().then(async connection => {
-        // register express routes from defined application routes
-        /*Routes.forEach(route => {
-            (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-                const result = (new (route.controller as any))[route.action](req, res, next);
-                if (result instanceof Promise) {
-                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+// app.use('/api', api)
 
-                } else if (result !== null && result !== undefined) {
-                    res.json(result);
-                }
-            });
-        });*/
+// createConnection()
+//     .then(async connection => {
+//         console.log('CONNECTED TO DATABASE');
 
-        app.listen(3000);
-        console.log("Express server has started on port 3000. Open http://localhost:3000/");
-    }).catch(error => {
-        console.log(error)
-    }
-);
+//         app.listen(3000);
+//         console.log("Express server has started on port 3000. Open http://localhost:3000/");
+//     })
+//     .catch(error => console.log(error));
+
+// // register express routes from defined application routes
+
+// // createConnection().then(async connection => {
+// //     const app = express();
+// //     app.use(bodyParser.json());
+
+// //     Routes.forEach(route => {
+// //         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+// //             const result = (new (route.controller as any))[route.action](req, res, next);
+// //             if (result instanceof Promise) {
+// //                 result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+// //             } else if (result !== null && result !== undefined) {
+// //                 res.json(result);
+// //             }
+// //         });
+// //     });
+    
+// //     app.listen(3000);
+
+// //     console.log("Express server has started on port 3000. Open http://localhost:3000");
+
+
+// // }).catch(error => console.log(error));
+import {Server} from './Server'
+const app = new Server();
+app.start();
